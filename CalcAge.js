bb8526a290d8369b46c9aa8843dc8ac684c04a33
anasalbrnsese <anasalbrnsese@hotmail.com>
2024-05-28 23:35:02 +0300

    Validation Age

--- CalcAge.js
+++ CalcAge.js
@@ -8,19 +8,66 @@ document.getElementById("born").addEventListener("click", function () {
   var currentMonth = today.getMonth() + 1; // Adding 1 because January is 0-indexed
   var currentDay = today.getDate();
 
-  var ageYears = currentYear - birthYear;
-  var ageMonths = currentMonth - birthMonth;
-  var ageDays = currentDay - birthDay;
+  let isValid = true;
 
-  if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
-    ageYears--;
-    ageMonths += 12;
+  // Validate year
+  if (isNaN(birthYear) || birthYear > currentYear || birthYear < 1900) {
+    setError(document.getElementById("year"), 'Provide a valid year');
+    isValid = false;
+  } else {
+    setSuccess(document.getElementById("year"));
   }
-  if (ageDays < 0) {
-    ageMonths--;
-    ageDays += new Date(currentYear, currentMonth, 0).getDate();
+
+  // Validate month
+  if (isNaN(birthMonth) || birthMonth < 1 || birthMonth > 12) {
+    setError(document.getElementById("month"), 'Provide a valid month');
+    isValid = false;
+  } else {
+    setSuccess(document.getElementById("month"));
   }
-  document.querySelector(".Year").innerText = ageYears + "";
-  document.querySelector(".Month").innerText = ageMonths + "";
-  document.querySelector(".Day").innerText = ageDays + "";
+
+  // Validate day
+  const daysInMonth = new Date(birthYear, birthMonth, 0).getDate();
+  if (isNaN(birthDay) || birthDay < 1 || birthDay > daysInMonth) {
+    setError(document.getElementById("day"), 'Provide a valid day');
+    isValid = false;
+  } else {
+    setSuccess(document.getElementById("day"));
+  }
+
+  if (isValid) {
+    var ageYears = currentYear - birthYear;
+    var ageMonths = currentMonth - birthMonth;
+    var ageDays = currentDay - birthDay;
+
+    if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
+      ageYears--;
+      ageMonths += 12;
+    }
+    if (ageDays < 0) {
+      ageMonths--;
+      ageDays += new Date(currentYear, currentMonth, 0).getDate();
+    }
+    document.querySelector(".Year").innerText = ageYears + "";
+    document.querySelector(".Month").innerText = ageMonths + "";
+    document.querySelector(".Day").innerText = ageDays + "";
+  }
 });
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+};
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
